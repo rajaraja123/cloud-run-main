b9174e8446a8f8f212036ae42fa9b3809ffc5611
rajaraja123 <rajapranatapanjaitan@gmail.com>
2023-05-08 22:43:57 +0700

    change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Testing');
+	res.send('Deployment');
 });
 
 const PORT = process.env.PORT || 8080;
